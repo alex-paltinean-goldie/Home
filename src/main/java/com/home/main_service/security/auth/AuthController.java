4f24e6b3955b05e2600048e62b420bdcbb3a3f47
Alex Paltinean <alex.paltinean@appointfix.com>
2025-02-02 18:36:43 +0200

    add signup

--- src/main/java/com/home/main_service/security/auth/AuthController.java
+++ src/main/java/com/home/main_service/security/auth/AuthController.java
@@ -1,29 +1,46 @@
 package com.home.main_service.security.auth;
 
 import com.home.main_service.security.JwtUtil;
+import com.home.main_service.user.User;
+import com.home.main_service.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final JwtUtil jwtUtil;
-
-    public AuthController(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest authRequest) {
-        // Dummy authentication, replace with actual authentication logic
-        if (true) {
+        if (userRepository.findByEmail(authRequest.getUsername()).isPresent()) {
             String token = jwtUtil.generateToken(authRequest.getUsername());
             return new AuthResponse(token);
         } else {
             throw new RuntimeException("Invalid credentials");
+        }
+    }
+
+    @PostMapping("/signup")
+    public AuthResponse signup(@RequestBody AuthRequest authRequest) {
+        if (userRepository.findByEmail(authRequest.getUsername()).isEmpty()) {
+            userRepository.save(User.builder()
+                    .email(authRequest.getUsername())
+                    .password(passwordEncoder.encode(authRequest.getPassword()))
+                    .role("ROLE_USER")
+                    .build());
+            String token = jwtUtil.generateToken(authRequest.getUsername());
+            return new AuthResponse(token);
+        } else {
+            throw new RuntimeException("User already present");
         }
     }
 }
