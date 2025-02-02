954f7f0a649209d44e6a6a2298ffa66a4859e3c5
Alex Paltinean <alex.paltinean@appointfix.com>
2025-02-02 17:54:13 +0200

    logs on public endpoint

--- src/main/java/com/home/main_service/publicc/PublicController.java
+++ src/main/java/com/home/main_service/publicc/PublicController.java
@@ -1,5 +1,8 @@
 package com.home.main_service.publicc;
 
+import com.home.main_service.security.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("public")
 public class PublicController {
+    private static final Logger logger = LoggerFactory.getLogger(PublicController.class);
 
     @GetMapping
     public PublicResponse getDefaults() {
+        logger.info("GET public");
         return new PublicResponse("testing2");
     }
 
