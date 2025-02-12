04c40b66154f2cbc37445dc9c1b1e470b4b2a671
Alex Paltinean <alex.paltinean@appointfix.com>
2025-02-12 23:13:02 +0200

    added deserializer for Instant

--- src/main/java/com/home/main_service/JacksonConfig.java
+++ src/main/java/com/home/main_service/JacksonConfig.java
@@ -1,6 +1,10 @@
 package com.home.main_service;
 
+import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
@@ -26,10 +30,19 @@ public class JacksonConfig {
         }
     }
 
+    public static class InstantDeserializer extends JsonDeserializer<Instant> {
+        @Override
+        public Instant deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+            String dateString = jsonParser.getText();
+            return Instant.from(FORMATTER.parse(dateString));
+        }
+    }
+
     @Bean
     public SimpleModule instantModule() {
         SimpleModule module = new SimpleModule();
         module.addSerializer(Instant.class, new InstantSerializer());
+        module.addDeserializer(Instant.class, new InstantDeserializer());
         return module;
     }
 }
